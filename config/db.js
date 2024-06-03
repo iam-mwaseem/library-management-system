1bb45fc3a4e085ea456f54be7f1bef2ab116653e
iam-mwaseem <dev.muhammad.waseem@gmail.com>
2024-06-03 14:49:27 +0500

    updates

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_PASSWORD,
   {
     host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USER,
+    username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     port: process.env.DATABASE_PORT,
